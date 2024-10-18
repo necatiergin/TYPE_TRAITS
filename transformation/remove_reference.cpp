230bb61b746e780d7b82badf3e56cc2a14eea0e6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-18 13:15:51 +0300

    Update remove_reference.cpp

--- transformation/remove_reference.cpp
+++ transformation/remove_reference.cpp
@@ -1,6 +1,6 @@
-template <typename T > struct remove_reference     {using type = T;};
-template <typename T > struct remove_reference<T&> {using type = T;};
-template <typename T > struct remove_reference<T&&>{using type = T;};
+template <typename T > struct remove_reference { using type = T; };
+template <typename T > struct remove_reference<T&> { using type = T; };
+template <typename T > struct remove_reference<T&&> { using type = T; };
 
 template <typename T>
 using remove_reference_t = typename remove_reference<T>::type;
@@ -8,4 +8,5 @@ using remove_reference_t = typename remove_reference<T>::type;
 int main()
 {
 	remove_reference_t<int&> x{ 3 };  //int x
-	remove_reference_t<int&&> y{ 3
+	remove_reference_t<int&&> y{ 3 }; //int y
+}
