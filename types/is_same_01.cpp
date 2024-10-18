e60a80e76b61cb6ff018aca44042b609c06c7d90
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-18 13:08:04 +0300

    Update and rename is_same_02.cpp to is_same_01.cpp

--- types/is_same_01.cpp
+++ types/is_same_01.cpp
@@ -1,8 +1,8 @@
+//std::is_same implementation
+
 #include <type_traits>
 #include <iostream>
 #include <cstdint>
-
-//is_same tür ilişkisi trait'inin gerçekleştirimi
 
 template<class T, class U>
 struct IsSame : std::false_type {};
@@ -16,8 +16,8 @@ inline constexpr bool IsSame_v = IsSame<T, U>::value;
 int main()
 {
 	std::cout << std::boolalpha;
-	std::cout << IsSame_v<int, int> << "\n"; //true
-	std::cout << IsSame_v<int, const int> << "\n"; //false
-	std::cout << IsSame_v<int, unsigned> << "\n"; //false
-	std::cout << IsSame_v<int, std::int32_t> << "\n"; //true or false
+	std::cout << IsSame_v<int, int> << '\n'; //true
+	std::cout << IsSame_v<int, const int> << '\n'; //false
+	std::cout << IsSame_v<int, unsigned> << '\n'; //false
+	std::cout << IsSame_v<int, std::int32_t> << '\n'; //true or false
 }
