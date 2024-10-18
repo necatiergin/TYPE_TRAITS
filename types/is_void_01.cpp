e3834243348a709f262a03231472725f74835413
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-18 13:11:23 +0300

    Update is_void_01.cpp

--- types/is_void_01.cpp
+++ types/is_void_01.cpp
@@ -6,10 +6,10 @@
 template <typename T>
 struct isVoid : std::false_type {};
 
-template<> struct isVoid<void> : std::true_type{};
-template<> struct isVoid<const void> : std::true_type{};
-template<> struct isVoid<volatile void> : std::true_type{};
-template<> struct isVoid<const volatile void> : std::true_type{};
+template<> struct isVoid<void> : std::true_type {};
+template<> struct isVoid<const void> : std::true_type {};
+template<> struct isVoid<volatile void> : std::true_type {};
+template<> struct isVoid<const volatile void> : std::true_type {};
 
 template <typename T>
 constexpr bool isVoid_v = isVoid<T>::value;
@@ -17,9 +17,9 @@ constexpr bool isVoid_v = isVoid<T>::value;
 int main()
 {
 	std::cout << std::boolalpha;
-	std::cout << isVoid_v<int> << "\n";
-	std::cout << isVoid_v<void> << "\n";
-	std::cout << isVoid_v<const void> << "\n";
-	std::cout << isVoid_v<void *> << "\n";
-	std::cout << isVoid_v<void const volatile> << "\n";
+	std::cout << isVoid_v<int> << '\n';
+	std::cout << isVoid_v<void> << '\n';
+	std::cout << isVoid_v<const void> << '\n';
+	std::cout << isVoid_v<void*> << '\n';
+	std::cout << isVoid_v<void const volatile> << '\n';
 }
